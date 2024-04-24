b1e031919ef209f43f59b8925a27d0054a5d0cc8
José Alejandro Guerra Montecino <108901711+JSotw@users.noreply.github.com>
2024-04-24 19:40:53 -0400

    Delete src/controllers/usuario.controller.js

--- src/controllers/usuario.controller.js
+++
@@ -1,9 +0,0 @@
-import usuarioModel from "../models/usuario.model.js";
-
-export const traerUsuarios = async (req, res) => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
